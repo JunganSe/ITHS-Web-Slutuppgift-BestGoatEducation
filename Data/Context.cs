d2e1016e5d11a30445f233c38f4563790c5c064a
Johan Ljungberg <jlj.9@hotmail.com>
2022-05-19 23:25:59 +0200

    Created context

--- Data/Context.cs
+++ Data/Context.cs
@@ -1,11 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WestcoastEducationApi.Models;
 
 namespace WestcoastEducationApi.Data;
 
-public class Context
+public class Context : DbContext
 {
+    public DbSet<Address> Addresses => Set<Address>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<City> Cities => Set<City>();
+    public DbSet<Competence> Competences => Set<Competence>();
+    public DbSet<Country> Countries => Set<Country>();
+    public DbSet<Course> Courses => Set<Course>();
+    public DbSet<PostalCode> PostalCodes => Set<PostalCode>();
+    public DbSet<Street> Streets => Set<Street>();
+    public DbSet<Student> Students => Set<Student>();
+    public DbSet<Teacher> Teachers => Set<Teacher>();
+    public DbSet<Student_Course> Student_Courses => Set<Student_Course>();
+    public DbSet<Teacher_Competence> Teacher_Competences => Set<Teacher_Competence>();
     
+    public Context(DbContextOptions options)
+        : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student_Course>()
+            .HasKey(sc => new { sc.StudentId, sc.CourseId });
+        
+        modelBuilder.Entity<Teacher_Competence>()
+            .HasKey(tc => new { tc.TeacherId, tc.CompetenceId });
+    }
 }
