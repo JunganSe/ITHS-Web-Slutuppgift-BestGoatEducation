fef9e4b4539f34a526570616ce84973c5ac655b8
Johan Ljungberg <jlj.9@hotmail.com>
2022-05-27 23:20:36 +0200

    Rudimentary login, started user registration

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,25 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using WestcoastEducationStudentApp.ViewModels;
 
 namespace WestcoastEducationStudentApp.Pages
 {
     public class Login : PageModel
     {
-        public Login()
+        private readonly IConfiguration _config;
+        private readonly string _apiUrl;
+        
+        [BindProperty]
+        public List<AppUserViewModel>? AppUsers { get; set; }
+        
+        public Login(IConfiguration config)
         {
+            _config = config;
+            _apiUrl = _config.GetValue<string>("ApiUrl");
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            string url = $"{_apiUrl}/AppUser";
+            var httpClient = new HttpClient();
+            AppUsers = await httpClient.GetFromJsonAsync<List<AppUserViewModel>>(url) ?? new List<AppUserViewModel>();
         }
 
-        public void OnPost()
+        public void OnPost(string userId, List<AppUserViewModel> appUsers)
         {
+            // HttpContext.Session.SetString("UserId", userId);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    // options.IdleTimeout = TimeSpan.FromSeconds(5);
+    // options.Cookie.HttpOnly = true;
+    // options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +27,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapRazorPages();
 
