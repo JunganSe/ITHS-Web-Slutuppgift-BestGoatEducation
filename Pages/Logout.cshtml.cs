c99bc87f4f7a60ef1fe066c814f20db112b04962
Johan Ljungberg <jlj.9@hotmail.com>
2022-05-28 15:04:46 +0200

    Logout

---
+++ Pages/Logout.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WestcoastEducationStudentApp.Pages;
+
+public class Logout : PageModel
+{
+    public Logout()
+    {
+    }
+
+    public void OnGet()
+    {
+        
+    }
+    
+    public void OnPost()
+    {
+        HttpContext.Session.Clear();
+    }
+}
