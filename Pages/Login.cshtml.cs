96d53f4b2df498e6698d2a6314422fa5a289fcfd
Johan Ljungberg <jlj.9@hotmail.com>
2022-05-28 14:54:03 +0200

    Store user name in session

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -18,15 +18,23 @@ namespace WestcoastEducationStudentApp.Pages
             _apiUrl = _config.GetValue<string>("ApiUrl");
         }
 
-        public async Task OnGet()
+        public async Task OnGetAsync()
         {
             string url = $"{_apiUrl}/AppUser";
             var httpClient = new HttpClient();
             AppUsers = await httpClient.GetFromJsonAsync<List<AppUserViewModel>>(url) ?? new List<AppUserViewModel>();
         }
 
-        public void OnPost(string userId, List<AppUserViewModel> appUsers)
+        public async Task OnPostAsync(string userId, List<AppUserViewModel> appUsers)
         {
+            var httpClient = new HttpClient();
+            string url = $"{_apiUrl}/AppUser/{userId}";
+            var userModel = await httpClient.GetFromJsonAsync<AppUserViewModel>(url);
+            if (userModel == null)
+                return;
+
+            string userNameFull = $"{userModel.FirstName} {userModel.LastName}";
+            HttpContext.Session.SetString("UserNameFull", userNameFull);
             HttpContext.Session.SetString("UserId", userId);
             Response.Redirect("./Index");
         }
