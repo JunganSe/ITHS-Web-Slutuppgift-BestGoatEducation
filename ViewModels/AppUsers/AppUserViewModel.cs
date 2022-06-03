bd17620a927608c8315279dd471d093571bacd1d
Johan Ljungberg <jlj.9@hotmail.com>
2022-06-03 22:46:13 +0200

    Students list

---
+++ ViewModels/AppUsers/AppUserViewModel.cs
@@ -0,0 +1,12 @@
+namespace WestcoastEducationAdminApp.ViewModels.AppUsers;
+
+public class AppUserViewModel
+{
+    public string? Id { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? AddressName { get; set; }
+    public int AddressId { get; set; }
+}
