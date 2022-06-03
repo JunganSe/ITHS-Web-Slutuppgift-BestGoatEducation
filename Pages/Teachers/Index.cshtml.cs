c379b72b3431fd750ffe3d946f27c076740c7f71
Johan Ljungberg <jlj.9@hotmail.com>
2022-06-03 22:56:42 +0200

    Teachers list

---
+++ Pages/Teachers/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WestcoastEducationAdminApp.ViewModels.AppUsers;
+using WestcoastEducationAdminApp.ViewModels.Courses;
+
+namespace WestcoastEducationAdminApp.Pages.Teachers;
+
+[BindProperties]
+public class Index : PageModel
+{
+    private readonly IConfiguration _config;
+    private readonly string _apiUrl;
+
+    public List<AppUserViewModel> TeacherModels { get; set; } = new();
+
+    public Index(IConfiguration config)
+    {
+        _config = config;
+        _apiUrl = _config.GetValue<string>("ApiUrl");
+    }
+
+    public async Task OnGetAsync()
+    {
+        var httpClient = new HttpClient();
+        string url = $"{_apiUrl}/AppUser/Teachers";
+        TeacherModels = await httpClient.GetFromJsonAsync<List<AppUserViewModel>>(url) ?? new List<AppUserViewModel>();
+    }
+}
