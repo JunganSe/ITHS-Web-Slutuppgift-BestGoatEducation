3bf190f9f68a79c46fce50c6348b423d41552814
Johan Ljungberg <jlj.9@hotmail.com>
2022-06-06 15:35:41 +0200

    Filter teachers by competence

--- Pages/Teachers/Index.cshtml.cs
+++ Pages/Teachers/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WestcoastEducationAdminApp.ViewModels.AppUsers;
+using WestcoastEducationAdminApp.ViewModels.Competences;
 using WestcoastEducationAdminApp.ViewModels.Courses;
 
 namespace WestcoastEducationAdminApp.Pages.Teachers;
@@ -8,19 +9,28 @@ namespace WestcoastEducationAdminApp.Pages.Teachers;
 [BindProperties]
 public class Index : PageModel
 {
-    private readonly string _apiUrl;
+	private readonly string _apiUrl;
 
-    public List<AppUserViewModel> TeacherModels { get; set; } = new();
+	public List<AppUserViewModel>? TeacherModels { get; set; }
+	public List<CompetenceViewModel>? CompetenceModels { get; set; }
 
-    public Index(IConfiguration config)
-    {
-        _apiUrl = config.GetValue<string>("ApiUrl");
-    }
+	public Index(IConfiguration config)
+	{
+		_apiUrl = config.GetValue<string>("ApiUrl");
+	}
 
-    public async Task OnGetAsync()
-    {
-        var httpClient = new HttpClient();
-        string url = $"{_apiUrl}/AppUser/Teachers";
-        TeacherModels = await httpClient.GetFromJsonAsync<List<AppUserViewModel>>(url) ?? new List<AppUserViewModel>();
-    }
+	public async Task OnGetAsync(string? byCompetence)
+	{
+		var httpClient = new HttpClient();
+		
+		string competenceUrl = $"{_apiUrl}/Competence";
+		CompetenceModels = await httpClient.GetFromJsonAsync<List<CompetenceViewModel>>(competenceUrl)
+			?? new List<CompetenceViewModel>();
+		
+		string teacherUrl = (string.IsNullOrEmpty(byCompetence))
+			? $"{_apiUrl}/AppUser/Teachers"
+			: $"{_apiUrl}/AppUser/TeachersByCompetence/{byCompetence}";
+		TeacherModels = await httpClient.GetFromJsonAsync<List<AppUserViewModel>>(teacherUrl)
+			?? new List<AppUserViewModel>();
+	}
 }
