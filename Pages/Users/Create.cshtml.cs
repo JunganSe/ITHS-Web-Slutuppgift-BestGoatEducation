4e910a27cebc7f1bcd50b77033f31d17928e919a
Johan Ljungberg <jlj.9@hotmail.com>
2022-06-09 09:34:13 +0200

    Can pick existing address when creating user

--- Pages/Users/Create.cshtml.cs
+++ Pages/Users/Create.cshtml.cs
@@ -8,41 +8,50 @@ namespace WestcoastEducationAdminApp.Pages.Users;
 [BindProperties]
 public class Create : PageModel
 {
-    private readonly string _apiUrl;
+	private readonly string _apiUrl;
 
-    public CreateAppUserViewModel UserModel { get; set; } = new();
-    public CreateAddressViewModel AddressModel { get; set; } = new();
-    
-    public Create(IConfiguration config)
-    {
-        _apiUrl = config.GetValue<string>("ApiUrl");
-    }
+	public CreateAppUserViewModel UserModel { get; set; } = new();
+	public List<AddressViewModel>? AddressModels { get; set; }
+	public CreateAddressViewModel CreateAddressModel { get; set; } = new();
+	
+	public Create(IConfiguration config)
+	{
+		_apiUrl = config.GetValue<string>("ApiUrl");
+	}
 
-    public void OnGet()
-    {
-    }
+	public async Task OnGetAsync()
+	{
+		var httpClient = new HttpClient();
+		string addressUrl = $"{_apiUrl}/Address";
+		AddressModels = await httpClient.GetFromJsonAsync<List<AddressViewModel>>(addressUrl)
+			?? new List<AddressViewModel>();
+	}
 
-    public async Task OnPostAsync()
-    {
-        var httpClient = new HttpClient();
-        string addressUrl = $"{_apiUrl}/Address";
-        var response = await httpClient.PostAsJsonAsync(addressUrl, AddressModel);
-        if (!response.IsSuccessStatusCode)
-        {
-            ViewData["Message"] = "Failed to create address";
-            return;
-        }
+	public async Task OnPostAsync()
+	{
+		var httpClient = new HttpClient();
+		
+		if (UserModel!.AddressId == 0)
+		{
+			string addressUrl = $"{_apiUrl}/Address";
+			var addressResponse = await httpClient.PostAsJsonAsync(addressUrl, CreateAddressModel);
+			if (!addressResponse.IsSuccessStatusCode)
+			{
+				ViewData["Message"] = "Failed to create address";
+				return;
+			}
+			UserModel.AddressId = int.Parse(await addressResponse.Content.ReadAsStringAsync());
+		}
 
-        UserModel.AddressId = int.Parse(await response.Content.ReadAsStringAsync());
-        string userUrl = $"{_apiUrl}/AppUser";
-        response = await httpClient.PostAsJsonAsync(userUrl, UserModel);
-        if (!response.IsSuccessStatusCode)
-        {
-            ViewData["Message"] = "Failed to create user";
-            return;
-        }
+		string userUrl = $"{_apiUrl}/AppUser";
+		var userResponse = await httpClient.PostAsJsonAsync(userUrl, UserModel);
+		if (!userResponse.IsSuccessStatusCode)
+		{
+			ViewData["Message"] = "Failed to create user";
+			return;
+		}
 
-        var userId = await response.Content.ReadAsStringAsync();
-        Response.Redirect($"/Users/Details?id={userId}");
-    }
+		var userId = await userResponse.Content.ReadAsStringAsync();
+		Response.Redirect($"/Users/Details?id={userId}");
+	}
 }
